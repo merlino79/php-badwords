608eb8862f53def8956bf9f77bf88195e5a15190
Merlino79 <alessandro.ciancio@hotmail.com>
2021-06-03 13:32:00 +0200

    partenza

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="styles/style.css">
+  <title>first_php</title>
+</head>
+<body>
+<p>
+  ciao
+</p>
+
+
+
+
+
+
+
+
+
+<!-- php -->
+<h1>
+<?php
+echo 'ciao arsenè'
+?>
+</h1>
+  
+</body>
+</html>
